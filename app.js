1ffd5536c412f241fc9a6d363ea64e1ce93634a2
Mark Prangan <pranganmark@gmail.com>
2023-08-23 01:29:09 +1000

    Added button that resets grid with amount per row and column that was prompted

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 const container = document.querySelector('#container');
+var grid;
 
-for (i = 0; i < 16; i++) {
-    const row = document.createElement('div')
-    row.className = 'row'
-    container.appendChild(row)
-    for (j = 0; j < 16; j++) {
-        const col = document.createElement('div')
-        col.className = 'col'
-        row.appendChild(col)
+function callPrompt() {
+    grid = prompt('Enter a number ?');
+    while (isNaN(grid) == true || grid < 0 || grid > 100) {
+        grid = prompt('how many do you want?');
     }
+    return grid
 }
 
-const boxes = document.querySelectorAll('.col')
-boxes.forEach((box) => {
-    box.addEventListener('mouseenter', () => {
-        box.style.backgroundColor = 'black';
+callPrompt()
+createGrid(grid)
+
+function createGrid(grid) {
+    for (i = 0; i < grid; i++) {
+        const row = document.createElement('div')
+        row.className = 'row'
+        container.appendChild(row)
+        for (j = 0; j < grid; j++) {
+            const col = document.createElement('div')
+            col.className = 'col'
+            row.appendChild(col)
+        }
+    }
+    makeEventListeners()
+}
+
+function clearGrid() {
+    container.innerHTML = ''
+}
+
+function makeEventListeners() {
+    const boxes = document.querySelectorAll('.col');
+    boxes.forEach((box) => {
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = 'black';
+        });
     });
+}
+
+const change = document.querySelector('#change');
+change.addEventListener('click', () => {
+    grid = callPrompt()
+    clearGrid()
+    createGrid(grid)
 });
