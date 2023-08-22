c9570cb8ee61588c314251f4ad335c3f885eeb93
Mark Prangan <pranganmark@gmail.com>
2023-08-23 01:44:26 +1000

    Made webpage automatically load a 16x16 grid

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ var grid;
 
 function callPrompt() {
     grid = prompt('Enter a number ?');
-    while (isNaN(grid) == true || grid < 0 || grid > 100) {
+    while (isNaN(grid) == true || grid < 1 || grid > 100) {
         grid = prompt('how many do you want?');
     }
     return grid
 }
 
-callPrompt()
-createGrid(grid)
+createGrid(16)
 
 function createGrid(grid) {
     for (i = 0; i < grid; i++) {
